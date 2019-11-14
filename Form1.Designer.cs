28f0590038cddf51f821eeb4b8478aaccd4f634c
Viktoria <vika.herman.97@gmail.com>
2019-11-14 20:45:43 +0100

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace laboInf4
+{
+    partial class FormFirst
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonChangeLabel = new System.Windows.Forms.Button();
+            this.labelNumberCounter = new System.Windows.Forms.Label();
+            this.Picture = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonChangeLabel
+            // 
+            this.buttonChangeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonChangeLabel.Location = new System.Drawing.Point(183, 298);
+            this.buttonChangeLabel.Name = "buttonChangeLabel";
+            this.buttonChangeLabel.Size = new System.Drawing.Size(133, 47);
+            this.buttonChangeLabel.TabIndex = 0;
+            this.buttonChangeLabel.Text = "Zmień napis";
+            this.buttonChangeLabel.UseVisualStyleBackColor = true;
+            this.buttonChangeLabel.Click += new System.EventHandler(this.buttonChangeLabel_Click);
+            // 
+            // labelNumberCounter
+            // 
+            this.labelNumberCounter.AutoSize = true;
+            this.labelNumberCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelNumberCounter.Location = new System.Drawing.Point(176, 29);
+            this.labelNumberCounter.Name = "labelNumberCounter";
+            this.labelNumberCounter.Size = new System.Drawing.Size(433, 39);
+            this.labelNumberCounter.TabIndex = 1;
+            this.labelNumberCounter.Text = "jeszcze nie zaczołeś liczyć";
+            // 
+            // Picture
+            // 
+            this.Picture.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Picture.Location = new System.Drawing.Point(422, 298);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(121, 47);
+            this.Picture.TabIndex = 2;
+            this.Picture.Text = "Picture";
+            this.Picture.UseVisualStyleBackColor = true;
+            this.Picture.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormFirst
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Picture);
+            this.Controls.Add(this.labelNumberCounter);
+            this.Controls.Add(this.buttonChangeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormFirst";
+            this.Text = "FirstAplication";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonChangeLabel;
+        private System.Windows.Forms.Label labelNumberCounter;
+        private System.Windows.Forms.Button Picture;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laboInf4
+{
+    public partial class FormFirst : Form
+    {
+        int licznik = 0;
+        public FormFirst()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonChangeLabel_Click(object sender, EventArgs e)
+        {
+            licznik++;
+            labelNumberCounter.Text = "Liczba klik: " + licznik;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormSecond forma = new FormSecond();
+            forma.Show();
+        }
+    }
+}
